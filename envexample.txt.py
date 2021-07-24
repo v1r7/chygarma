7500c3c3dbd229805f91f4ad7e2cb8cb12b5ab49
v1r7 <gubaevmikhail@gmail.com>
2021-07-24 17:17:08 +0600

    env example

---
+++ envexample.txt.py
@@ -0,0 +1,9 @@
+SECRET_KEY = djangosecretkey
+DEBUG = True
+ALLOWED_HOSTS = *
+DB_ENGINE = django.db.backends.postgresql_psycopg2
+DB_NAME = chygarmatemplate
+DB_USER = postgres
+DB_PASSWORD = postgres
+DB_HOST = 127.0.0.1
+DB_PORT = 5432
